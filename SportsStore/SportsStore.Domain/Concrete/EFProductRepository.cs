79a969a6f386bac1ffe146ec1ba4f5065f1496fe
Paul McLaughlin <paul@themclaughlins.me>
2013-03-19 22:46:42 +0000

    Added in admin section to view and edit products

--- SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using System.Data;
 
 namespace SportsStore.Domain.Concrete
 {
@@ -13,6 +14,21 @@ namespace SportsStore.Domain.Concrete
         public IQueryable<Product> Products
         {
             get { return context.Products; }
+        }
+
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                context.Entry(product).State = EntityState.Modified;
+            }
+
+            context.SaveChanges();
         }
     }
 }
