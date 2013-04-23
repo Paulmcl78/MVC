8140d6ae902b702a46795db6c02b0499c7b95dd9
Paul McLaughlin <pmclaughlin@navinet.net>
2013-04-23 17:34:52 +0100

    AJAX authorize attribute

---
+++ MvcFilters/MvcFilters/Infractructure/AjaxAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFilters.Infractructure
+{
+    public class AjaxAuthorizeAttribute : AuthorizeAttribute
+    {
+
+        /// <summary>
+        /// When this filter detects an AJAX request, it responds with JSON data that reports the
+        /// problem and provides the URL for the login page.
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                UrlHelper urlHelper = new UrlHelper(filterContext.RequestContext);
+                filterContext.Result = new JsonResult
+                {
+                    Data = new
+                    {
+                        Error = "NotAuthorized",
+                        LogOnUrl = urlHelper.Action("LogOn", "Account")
+                    },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
