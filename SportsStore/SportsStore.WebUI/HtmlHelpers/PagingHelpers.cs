9ed27bbcd3794743d0c4e150cc30321d42e0b365
Paul McLaughlin <paul@themclaughlins.me>
2013-04-23 23:41:23 +0100

    Playing with twitter bootstrap in the sports store

--- SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
+++ SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -13,15 +13,19 @@ namespace SportsStore.WebUI.HtmlHelpers
         public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
         {
             StringBuilder result = new StringBuilder();
+            result.Append("<ul>");
             for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
+                result.Append("<li>");
               TagBuilder tag = new TagBuilder("a"); // constructs an <a> tag
               tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
                 if (i == pagingInfo.CurrentPage)
                 tag.AddCssClass("selected");
                 result.Append(tag.ToString());
+                result.Append("</li>");
             }
+            result.Append("</ul>");
 
             return MvcHtmlString.Create(result.ToString());
         }
