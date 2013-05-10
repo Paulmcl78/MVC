2c645f3cfb7389ec264a66f5dce0f29ba48abc6c
Paul McLaughlin <pmclaughlin@navinet.net>
2013-05-10 15:23:04 +0100

    exendable controllers

---
+++ ControllerExtensibility/ControllerExtensibility/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            // Use LocalDB for Entity Framework by default
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+
+            //This is to register your custom controller factory
+            //ControllerBuilder.Current.SetControllerFactory( new CustomControllerFactory());
+
+            //We can use the controller builder method to add namespaces that should be given priority
+            //The order in which we add the namespaces does not implat any kind of serach order.
+            //ControllerBuilder.Current.DefaultNamespaces.Add("MyControllerNamespace");
+            //ControllerBuilder.Current.DefaultNamespaces.Add("MyProject.*");
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ ControllerExtensibility/ControllerExtensibility/Infrastructure/CustomControllerFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+using ControllerExtensibility.Controllers;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    /// <summary>
+    /// It is not recommended that you create you own factory but more extend the default.
+    /// </summary>
+    public class CustomControllerFactory : IControllerFactory
+    {
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            Type targetType = null;
+
+            switch (controllerName)
+            {
+                case "Home":
+                    //we change the route data as views are displayed based on the route controller name
+                    //not the controller class name, so to route this elsewhere we need to ensure we are
+                    //also changing the routing data so that we do not break anythin further down the line.
+                    requestContext.RouteData.Values["controller"] = "First";
+                    targetType = typeof (FirstController);
+                    break;
+                case "First":
+                    targetType = typeof (FirstController);
+                    break;
+                case "Second":
+                    targetType = typeof (SecondController);
+                    break;
+            }
+
+            return targetType == null ? null : (IController) Activator.CreateInstance(targetType);
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+           return SessionStateBehavior.Default;
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            IDisposable disposable = controller as IDisposable;
+
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
